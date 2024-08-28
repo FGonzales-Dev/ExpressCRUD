7cbf2b1c252524f52cc569378777b88fbbfa7756
francis gonzales <fgonzales.private@icloud.com>
2024-08-28 11:05:23 +0800

    fix cors

--- app.js
+++ app.js
@@ -10,13 +10,16 @@ import { router } from "./router/Routes.js";
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(express.json());
 
-app.use("/api", router);
-
 app.use((req, res, next) => {
   res.header("Access-Control-Allow-Origin", "*");
   res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+  res.header("Access-Control-Allow-Methods", "GET, POST, PUT, DELETE");
   next();
 });
+
+app.use("/api", router);
+
+
 
 app.listen(PORT, () => {
   console.log(`Server is running on PORT: http://localhost:${PORT}`);
