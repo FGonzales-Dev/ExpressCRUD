260ea0dbb0da7be8530f2fbe72a0df22970d2df5
francis gonzales <fgonzales.private@icloud.com>
2024-08-28 10:54:11 +0800

    added cors

--- app.js
+++ app.js
@@ -12,6 +12,12 @@ app.use(express.json());
 
 app.use("/api", router);
 
+app.use((req, res, next) => {
+  res.header("Access-Control-Allow-Origin", "*");
+  res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+  next();
+});
+
 app.listen(PORT, () => {
   console.log(`Server is running on PORT: http://localhost:${PORT}`);
 });
